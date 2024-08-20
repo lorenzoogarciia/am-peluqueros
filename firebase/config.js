a88c757e726b8670915570e1a83a1ffdb9e3976a
lorenzoogarciia_ <lorenzogarciagarcia03@gmail.com>
2024-08-20 17:32:33 +0200

    No message

--- firebase/config.js
+++ firebase/config.js
@@ -4,15 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getReactNativePersistence } from "firebase/auth/react-native";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyBCGr9mJyleM5RIRoYM2vOMv05DaqmMBHY",
-  authDomain: "ampeluqeros.firebaseapp.com",
-  projectId: "ampeluqeros",
-  storageBucket: "ampeluqeros.appspot.com",
-  messagingSenderId: "389220204116",
-  appId: "1:389220204116:web:1a3bad2f132b12f4b2f7c1",
-  measurementId: "G-FXLLZ4S9W7",
-};
+//Configuracion de Firebase
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
